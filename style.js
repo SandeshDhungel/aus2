a2e6e921e87d3e6bf6275d0bee18605fdd9937e2
Sandesh Dhungel <41162668+SandeshDhungel@users.noreply.github.com>
2020-03-03 16:52:35 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+function increaseIconSize() {
+  var Icon = document.getElementById("homeIcon");
+  Icon.style = "width:50px;height:50px";
+}
+
+function decreaseIconSize() {
+  var Icon = document.getElementById("homeIcon");
+  Icon.style = "width:40px;height:40px";
+}
